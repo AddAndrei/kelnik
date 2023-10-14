a6581df80882f99d90788fb871ac5494adb76d80
AddAndrei <andrei.add@yandex.ru>
2023-10-14 11:56:19 +0300

    feat

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\NotesController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Auth;
-
-Route::get('/', [NotesController::class, 'index'])->name("index");
-Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home/add', [HomeController::class, 'showAddNoteForm'])->name('note.add');
-Route::post('/home', [HomeController::class, 'storeNote'])->name('note.store');
-Route::get('/home/{note}/edit', [HomeController::class, 'showEditNoteForm'])->name('note.edit')->middleware('can:update,note');
-Route::patch('/home/{note}', [HomeController::class, 'updateNote'])->name('note.update')->middleware('can:update,note');
-Route::get("/home/{note}/delete", [HomeController::class, 'showDeleteNoteForm'])->name('note.delete')->middleware('can:destroy,note');
-Route::delete("/home/{note}", [HomeController::class, 'destroyNote'])->name('note.destroy')->middleware('can:destroy,note');
-Route::get('/{note}', [NotesController::class, 'detail'])->name("detail");
+Route::get('/', function () {
+    return view('welcome');
+});
